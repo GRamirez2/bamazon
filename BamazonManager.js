1c4c8da45fd5c17e6acbd2fb260e1b39dfb316ed
George Ramirez <georgeramirez@georges-mbp.gateway.pace.com>
2016-10-26 16:45:57 -0500

    first two choices in manager complete

--- BamazonManager.js
+++ BamazonManager.js
@@ -1,10 +1,8 @@
 
-// need to install the NPM mysql package
 var mysql = require('mysql');
 var inquirer = require ('inquirer');
 var time = new Date().toDateString();
 
-// setting up your connection to the local host
 var connection = mysql.createConnection({
 
     host: 'localhost',
@@ -24,16 +22,9 @@ connection.connect(function(err) {
 })//end of conection.connect
 
 var init = function(){
-    console.log('\n***** Hello and welcome to "Bamazon" 10.23.GR, today is '+time+' *****\n');
+    console.log('\n***** Hello and welcome to "Bamazon for Managers" 10.23.GR, today is '+time+' *****\n');
 
-        connection.query('SELECT ItemID, ProductName, Price FROM products', function(err, res){
-            if (err) throw err;
-            res.forEach(function(item,index){
-                console.log(item.ProductName+", "+"$"+item.Price+",  ID=("+item.ItemID+")\n");
-                });
-                begin();
-        });
-
+    begin();
 
 }//endo of init function
 
@@ -41,36 +32,29 @@ var init = function(){
 // Begin app if connection ok with no errors
 var begin = function (){
     
-    inquirer.prompt([{
+    inquirer.prompt({
 
-                type : 'input',
-                name : "id",
-                message : "Enter the ID=(number) for the item you would like to purchase."
-
-
-                },
-                {
-                name : "quanity",
-                message : "Excellent choice! How many would you like to buy?"
+                type : 'list',
+                name : "task",
+                message : "Hello Manager, what would you like to do today?",
+                choices : ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
                 
-            }]).then(function(answer){
-                    //console.log(answer.id)
-                    var id = answer.id;
-                    var quanity = answer.quanity
-                    // console.log(quanity)
-                    var query = 'SELECT StockQuantity FROM products WHERE?' 
-                    connection.query(query,{itemID : id}, function(err, res){
-                        if (err) throw err;
-                        // console.log(res[0].StockQuantity)
-                        var quanityNum = parseInt(res[0].StockQuantity);
-                        // console.log("quantityNum = "+quanityNum)
-                        if (quanity <= quanityNum){
-                            buy(id, quanityNum, quanity);
-                        }else{
-                            console.log ("\n********** OH NO! ***********\nSorry, but our inventory appears to be low on this item. We can not fullfill your request.\n Please select fewer items for your order or another item")
-                            init();
-                        };
-                    });
+            }).then(function(answer){
+                switch (answer.task){
+                    case "View Products for Sale" : forSale();
+                    break;
+
+                    case "View Low Inventory" : lowInventory();
+                    break;
+
+                    case "Add to Inventory" : addInventory();
+                    break;
+
+                    case "Add New Product" : addProduct();
+                    break;
+
+                }//end of switch
+                   
             });//end of answer promise
 
 };//end of begin function
@@ -103,4 +87,42 @@ function buy (id, quanityNum, quanity){
 return;
 };//end of buy function
 
+function forSale(){
+    connection.query('Select * FROM Products', function(err,res){
+        if (err)throw err;
+        console.log("=========================Items FOR SALE=========================")
+        res.forEach(function(item){
+            console.log("ID#= "+item.ItemID+" "+" Product= "+item.ProductName+" "+"  Department= "+item.DepartmentName+" "+"  Price= "+item.Price+" "+"  Inventory= "+item.StockQuantity+"\n")
+      
+        })
+        // console.log(res)
+        console.log("======================END of FOR SALE Items====================\n")
+        begin();
+    });
+    
+}
+
+function lowInventory(){
+    connection.query('SELECT StockQuantity, ProductName FROM Products', function (err, res){
+        if(err)throw err;
+        console.log("=========================LOW INVENTORY Items=========================\n")
+        res.forEach(function(item){
+            var itemStock = parseInt(item.StockQuantity)
+                if(itemStock <= 5){
+                    console.log('Item= '+item.ProductName+', Stock Quantity= '+itemStock+'\n')
+                }
+        })
+        console.log("=========================LOW INVENTORY Items=========================\n")
+        begin();
+    })
+}
+
+function addInventory(){
+    console.log('this is the addInventory function');
+    connection.end();
+}
+
+function addProduct(){
+    console.log('this is the addProduct function');
+}
 
